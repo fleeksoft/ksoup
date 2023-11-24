bb98604113bf5b414ea24b1432a22ab02a71a861
sabeeh <mr.hussnain.it@gmail.com>
2023-11-24 04:37:57 +0400

    BufferReader allow charset encoder and refactor function getActiveSource to getSource

--- ksoup/src/commonMain/kotlin/com/fleeksoft/ksoup/ported/BufferReader.kt
+++ ksoup/src/commonMain/kotlin/com/fleeksoft/ksoup/ported/BufferReader.kt
@@ -1,6 +1,7 @@
 package com.fleeksoft.ksoup.ported
 
-import io.ktor.utils.io.core.toByteArray
+import io.ktor.utils.io.charsets.*
+import io.ktor.utils.io.core.*
 import okio.Buffer
 import okio.BufferedSource
 import okio.Closeable
@@ -9,63 +10,89 @@ import okio.IOException
 public open class BufferReader : Closeable {
     // TODO: optimize it may be using single bytearray
     // TODO: buffer reader size limit
-    private val source: BufferedSource
+    private val _source: BufferedSource
     private var closed: Boolean = false
+    private var _charset: Charset? = null
 
 
     public constructor() {
-        this.source = Buffer()
+        this._source = Buffer()
     }
 
     public constructor(buffer: BufferedSource) {
-        this.source = buffer
+        this._source = buffer
     }
 
     // TODO: not sure if copy it or direct assign it
-    public constructor(bufferReader: BufferReader) : this(bufferReader.source)
+    public constructor(bufferReader: BufferReader) : this(bufferReader._source)
 
     public constructor(byteArray: ByteArray) {
         val buffer = Buffer()
         buffer.write(byteArray)
-        source = buffer
+        _source = buffer
     }
 
 
     public constructor(data: String) : this(data.toByteArray())
 
-    public fun remaining(): Int = getActiveSource().buffer.size.toInt()
+    public fun remaining(): Int = getSource().buffer.size.toInt()
 
-    public fun exhausted(): Boolean = getActiveSource().exhausted()
+    public fun exhausted(): Boolean = getSource().exhausted()
 
-    public fun getActiveSource(): BufferedSource {
+    private fun getSource(): BufferedSource {
         if (closed) {
             throw IOException("Buffer closed!")
         }
 
-        return source
+        return _source
     }
 
-    public fun size(): Long = source.buffer.size
+    public fun getBuffer(): BufferReader {
+        if (closed) {
+            throw IOException("Buffer closed!")
+        }
+
+        return BufferReader(_source)
+    }
+
+    public fun size(): Long = _source.buffer.size
 
     public open fun read(sink: ByteArray, offset: Int, byteCount: Int): Int {
-        return getActiveSource().read(sink = sink, offset = offset, byteCount = byteCount)
+        if (_charset != null) {
+            val byteArray = ByteArray(sink.size)
+            val result = getSource().read(sink = byteArray, offset = offset, byteCount = byteCount)
+            io.ktor.utils.io.core.String(bytes = byteArray, charset = _charset!!).toByteArray().copyInto(sink)
+            return result
+        }
+        return getSource().read(sink = sink, offset = offset, byteCount = byteCount)
     }
 
     public open fun read(): Int {
-        return getActiveSource().readByte().toInt()
+        return getSource().readByte().toInt()
     }
 
     public open fun readByteArray(): ByteArray {
-        return getActiveSource().readByteArray()
+        /*String(
+                    bufferReader.readByteArray(),
+                    charset = Charset.forName(charsetName)
+                )*/
+        var byteArray = getSource().readByteArray()
+        if (_charset != null) {
+            byteArray = io.ktor.utils.io.core.String(
+                bytes = byteArray,
+                charset = _charset!!
+            ).toByteArray()
+        }
+        return byteArray
     }
 
     public fun skip(byteCount: Long) {
-        getActiveSource().skip(byteCount)
+        getSource().skip(byteCount)
     }
 
     public fun skipFirstUnicodeChar(length: Int) {
         //        ignore x characters which can be 2-4 byte each for UTF-8
-        val firstByte = getActiveSource().peek().readByte().toInt()
+        val firstByte = getSource().peek().readByte().toInt()
 
         // Determine the length of the first character in UTF-8
         val firstCharLength = when {
@@ -77,30 +104,42 @@ public open class BufferReader : Closeable {
         }
 
         // Skip the first character and return the rest of the array
-        getActiveSource().skip((firstCharLength * length).toLong())
+        getSource().skip((firstCharLength * length).toLong())
     }
 
-    public fun getPeek(): BufferedSource {
-        return getActiveSource().peek()
+    public fun getPeek(): BufferReader {
+        val bufferReader = BufferReader(getSource().peek())
+        bufferReader._charset = this._charset
+        return bufferReader
     }
 
     public open fun read(b: ByteArray): Int = this[b]
 
     public operator fun get(pos: Int): Byte {
-        return getActiveSource().buffer[pos.toLong()]
+        return getSource().buffer[pos.toLong()]
     }
 
     public operator fun get(byteArray: ByteArray): Int {
 
-        return getActiveSource().read(byteArray)
+        return getSource().read(byteArray)
     }
 
 
-    public fun readString(): String = getActiveSource().readByteArray().decodeToString()
+    public fun readString(): String {
+        if (_charset != null) {
+            return io.ktor.utils.io.core.String(bytes = getSource().readByteArray(), charset = _charset!!)
+        }
+        return getSource().readByteArray().decodeToString()
+    }
+
     public fun readCharArray(): CharArray = readString().toCharArray()
 
     override fun close() {
         closed = true
-        source.close()
+        _source.close()
+    }
+
+    public fun setCharSet(charset: String) {
+        this._charset = Charset.forName(charset)
     }
 }
