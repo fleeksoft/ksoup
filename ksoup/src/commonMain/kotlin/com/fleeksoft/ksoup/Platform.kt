f5b5bb87521b65e3a037b753751851e5da205927
itboy87 <mr.hussnain.it@gmail.com>
2023-12-04 19:30:59 +0500

    regex `?i` is not supported by js replace it with IGNORE_CASE flag

--- ksoup/src/commonMain/kotlin/com/fleeksoft/ksoup/Platform.kt
+++ ksoup/src/commonMain/kotlin/com/fleeksoft/ksoup/Platform.kt
@@ -1,5 +1,6 @@
 package com.fleeksoft.ksoup
 
+import com.fleeksoft.ksoup.select.Evaluator
 import okio.BufferedSource
 import okio.Path
 
@@ -7,3 +8,15 @@ internal expect fun readGzipFile(file: Path): BufferedSource
 
 
 internal expect fun readFile(file: Path): BufferedSource
+
+
+
+// js don't support ?i
+// TODO: may be check this for js only and replace it
+internal fun jsSupportedRegex(regex: String): Regex  {
+    return if (regex.contains("(?i)")) {
+        Regex(regex.replace("(?i)", ""), RegexOption.IGNORE_CASE)
+    } else {
+        Regex(regex)
+    }
+}
