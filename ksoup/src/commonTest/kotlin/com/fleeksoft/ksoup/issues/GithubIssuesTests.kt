a5cdfb14203637d305efe575fa4fcbf28e07ba11
sabeeh <mr.hussnain.it@gmail.com>
2023-12-21 13:50:23 +0500

    test updated

--- ksoup/src/commonTest/kotlin/com/fleeksoft/ksoup/issues/GithubIssuesTests.kt
+++ ksoup/src/commonTest/kotlin/com/fleeksoft/ksoup/issues/GithubIssuesTests.kt
@@ -2,6 +2,7 @@ package com.fleeksoft.ksoup.issues
 
 import com.fleeksoft.ksoup.Ksoup
 import com.fleeksoft.ksoup.TestHelper
+import com.fleeksoft.ksoup.runTest
 import okio.Path.Companion.toPath
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -10,16 +11,19 @@ class GithubIssuesTests {
     @Test
     fun testIssue20DuplicateElements() {
         //    https://github.com/fleeksoft/ksoup/issues/20
-        Ksoup.parse(TestHelper.getFileAsString(TestHelper.getResourceAbsolutePath("htmltests/issue20.html.gz").toPath()))
-            .apply {
-                body().select("div[class=firs l]")
-                    .firstOrNull()?.let { element ->
-                        val titles = element.select("div[class=dtit]")
-                        val contents = element.select("div[class=img]")
-                        println("titles: ${titles.size}, contents: ${contents.size}")
-                        assertEquals(6, titles.size)
-                        assertEquals(6, contents.size)
-                    }
-            }
+        runTest {
+            Ksoup.parse(TestHelper.getFileAsString(TestHelper.getResourceAbsolutePath("htmltests/issue20.html.gz").toPath()))
+//            Ksoup.parseGetRequest("https://www.dm530w.org/")
+                .apply {
+                    body().select("div[class=firs l]")
+                        .firstOrNull()?.let { element ->
+                            val titles = element.select("div[class=dtit]")
+                            val contents = element.select("div[class=img]")
+                            println("titles: ${titles.size}, contents: ${contents.size}")
+                            assertEquals(6, titles.size)
+                            assertEquals(6, contents.size)
+                        }
+                }
+        }
     }
 }
