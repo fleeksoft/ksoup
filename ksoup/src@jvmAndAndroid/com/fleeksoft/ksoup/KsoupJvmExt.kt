d2a7681ff6f629e1c22a2ed2f8eac25ef627b2f7
sabeeh <mr.hussnain.it@gmail.com>
2024-07-31 16:33:11 +0500

    java add file parse

--- ksoup/src@jvmAndAndroid/com/fleeksoft/ksoup/KsoupJvmExt.kt
+++ ksoup/src@jvmAndAndroid/com/fleeksoft/ksoup/KsoupJvmExt.kt
@@ -3,6 +3,7 @@ package com.fleeksoft.ksoup
 import com.fleeksoft.ksoup.nodes.Document
 import com.fleeksoft.ksoup.parser.Parser
 import korlibs.io.stream.openSync
+import java.io.File
 import java.io.InputStream
 
 /**
@@ -24,6 +25,31 @@ public fun Ksoup.parseInputStream(
     return parse(
         // TODO: use syncstream without reading bytes
         syncStream = inputStream.readAllBytes().openSync(),
+        charsetName = charsetName,
+        baseUri = baseUri,
+        parser = parser,
+    )
+}
+
+
+/**
+ * Parse the contents of a file as HTML.
+ *
+ * @param file file to read.
+ * @param charsetName (optional) character set of file contents. Set to `null` to determine from `http-equiv` meta tag, if
+ * present, or fall back to `UTF-8` (which is often safe to do).
+ * @param baseUri  The URL where the HTML was retrieved from, to resolve relative links against.
+ * @return sane HTML
+ */
+
+public fun Ksoup.parseFile(
+    file: File,
+    charsetName: String? = null,
+    baseUri: String = file.absolutePath,
+    parser: Parser = Parser.htmlParser(),
+): Document {
+    return parse(
+        syncStream = file.openSync(),
         charsetName = charsetName,
         baseUri = baseUri,
         parser = parser,
