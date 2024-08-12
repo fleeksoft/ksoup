b820e2c1340238cecfa6e7a1b94badce66552bed
sabeeh <mr.hussnain.it@gmail.com>
2024-08-13 01:03:15 +0500

    add Charsets placeholder class

---
+++ ksoup-io/src/com/fleeksoft/ksoup/ported/io/Charsets.kt
@@ -0,0 +1,7 @@
+package com.fleeksoft.ksoup.ported.io
+
+object Charsets {
+    val UTF8 : Charset = TODO("placeholder replace from actual module")
+
+    fun forName(name: String): Charset = TODO("placeholder replace from actual module")
+}
