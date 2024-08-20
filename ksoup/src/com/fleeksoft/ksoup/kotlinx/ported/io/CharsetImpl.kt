031bd2f36b08cd502f0afac951862b72108854c2
sabeeh <mr.hussnain.it@gmail.com>
2024-08-20 09:49:03 +0500

    handle invalid sequence of bytes when decoding

--- ksoup/src/com/fleeksoft/ksoup/kotlinx/ported/io/CharsetImpl.kt
+++ ksoup/src/com/fleeksoft/ksoup/kotlinx/ported/io/CharsetImpl.kt
@@ -1,16 +1,14 @@
 package com.fleeksoft.ksoup.kotlinx.ported.io
 
 import com.fleeksoft.ksoup.ported.io.Charset
-import io.ktor.utils.io.charsets.Charsets
-import io.ktor.utils.io.charsets.decode
-import io.ktor.utils.io.charsets.forName
-import io.ktor.utils.io.charsets.name
-import io.ktor.utils.io.core.toByteArray
+import io.ktor.utils.io.charsets.*
+import io.ktor.utils.io.core.*
 import kotlinx.io.Buffer
 
 
 class CharsetImpl(override val name: String) : Charset {
     private var charset: io.ktor.utils.io.charsets.Charset = Charsets.forName(name)
+    private val charsetDecoder by lazy { this.charset.newDecoder() }
 
     constructor(charset: io.ktor.utils.io.charsets.Charset) : this(charset.name) {
         this.charset = charset
@@ -20,8 +18,16 @@ class CharsetImpl(override val name: String) : Charset {
         if (end <= 0) return 0
         val buffer = Buffer().apply { write(byteArray, start, end) }
         val size = buffer.size
-        stringBuilder.append(this.charset.newDecoder().decode(buffer))
-        return (size - buffer.size).toInt()
+        val strSizeBeforeDecode = stringBuilder.length
+        charsetDecoder.decode(buffer, stringBuilder, buffer.size.toInt())
+        val decodedBytes = (size - buffer.size).toInt()
+        var invalidBytes = 0
+        if (stringBuilder.lastOrNull()?.code == 65533) {
+            stringBuilder.setLength(stringBuilder.length - 1)
+            val validDecodedStrBytesSize = stringBuilder.substring(strSizeBeforeDecode).toByteArray(charset).size
+            invalidBytes = decodedBytes - validDecodedStrBytesSize
+        }
+        return decodedBytes - invalidBytes
     }
 
     override fun toByteArray(value: String): ByteArray {
