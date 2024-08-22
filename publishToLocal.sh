253260273eb857f49cd0871304524c5f4683e0b9
sabeeh <mr.hussnain.it@gmail.com>
2024-08-22 10:46:38 +0500

    publishToLocal script

---
+++ publishToLocal.sh
@@ -0,0 +1,11 @@
+./gradlew clean
+./gradlew :ksoup-engine-common:publishToMavenLocal
+./gradlew :ksoup-engine-kotlinx:publishToMavenLocal
+./gradlew :ksoup-engine-korlibs:publishToMavenLocal
+
+./gradlew clean
+./gradlew :ksoup:publishToMavenLocal -PisKorlibs=false
+
+./gradlew clean
+./gradlew :ksoup:publishToMavenLocal -PisKorlibs=true
+./gradlew :ksoup-network-korlibs:publishToMavenLocal -PisKorlibs=true
