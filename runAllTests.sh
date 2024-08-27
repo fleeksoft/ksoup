f2f25368cfd7ae262dc538da010ce64a6ea18d8f
sabeeh <mr.hussnain.it@gmail.com>
2024-08-28 00:26:39 +0500

    fix and update order of tests

--- runAllTests.sh
+++ runAllTests.sh
@@ -10,16 +10,16 @@ run_tests() {
     echo "Running tests with libBuildType=$libBuildType..."
 
     ./gradlew clean
-    ./gradlew jvmTest testDebugUnitTest testReleaseUnitTest -PlibBuildType=$libBuildType
-    ./gradlew iosX64Test iosSimulatorArm64Test macosX64Test macosArm64Test tvosX64Test tvosSimulatorArm64Test -PlibBuildType=libBuildType
+    ./gradlew jvmTest testDebugUnitTest testReleaseUnitTest -PlibBuildType="$libBuildType"
     rm -rf kotlin-js-store
-    ./gradlew jsTest wasmTest -PlibBuildType=$libBuildType
+    ./gradlew jsTest wasmTest -PlibBuildType="$libBuildType"
+    ./gradlew iosX64Test iosSimulatorArm64Test macosX64Test macosArm64Test tvosX64Test tvosSimulatorArm64Test -PlibBuildType="$libBuildType"
 }
-
-# Run tests for korlibs
-#run_tests korlibs
 
 # Run tests for kotlinx
 run_tests kotlinx
+
+# Run tests for korlibs
+run_tests korlibs
 
 echo "All tests run successfully!"
