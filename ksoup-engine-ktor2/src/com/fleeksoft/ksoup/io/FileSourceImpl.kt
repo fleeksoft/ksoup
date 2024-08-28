dea504dfd55dfbb5c01ae5bcebebf3a8b9283565
sabeeh <mr.hussnain.it@gmail.com>
2024-08-29 00:20:02 +0500

    add ktor2

---
+++ ksoup-engine-ktor2/src/com/fleeksoft/ksoup/io/FileSourceImpl.kt
@@ -0,0 +1,31 @@
+package com.fleeksoft.ksoup.io
+
+import kotlinx.io.buffered
+import kotlinx.io.files.Path
+import kotlinx.io.files.SystemFileSystem
+
+class FileSourceImpl : FileSource {
+    private val path: Path
+
+    private val sourceBuffered by lazy { buffered() }
+
+    constructor(file: Path) {
+        this.path = file
+    }
+
+    constructor(filePath: String) {
+        this.path = Path(filePath)
+    }
+
+    override suspend fun toSourceReader(): SourceReader = SourceReader.from(sourceBuffered)
+
+    private fun FileSourceImpl.buffered() = SystemFileSystem.source(path).buffered()
+
+    override fun getPath(): String {
+        return this.path.name
+    }
+
+    override fun getFullName(): String {
+        return this.path.name
+    }
+}
