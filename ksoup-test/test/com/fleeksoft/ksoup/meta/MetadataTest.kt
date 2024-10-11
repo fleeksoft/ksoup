2a5b4f051386dbac5422600fb8118fce18f7b2bd
Sabeeh Ul Hussnain <mr.hussnain.it@gmail.com>
2024-10-11 08:37:17 +0500

    improve parseMetaData and add shortcutIcon

--- ksoup-test/test/com/fleeksoft/ksoup/meta/MetadataTest.kt
+++ ksoup-test/test/com/fleeksoft/ksoup/meta/MetadataTest.kt
@@ -21,6 +21,7 @@ class MetadataTest {
                     <meta name="description" content="Test Description">
                     <link rel="canonical" href="https://example.com">
                     <link rel="icon" href="/favicon.ico">
+                    <link href="/pikacon-32x32.png" rel="shortcut icon" type="image/png">
                 </head>
             </html>
         """.trimIndent()
@@ -61,6 +62,58 @@ class MetadataTest {
         assertEquals("Test Description", metaData.description)
         assertEquals("https://example.com", metaData.canonical)
         assertEquals("https://example.com/favicon.ico", metaData.favicon)
+        assertEquals("https://example.com/pikacon-32x32.png", metaData.shortcutIcon)
+    }
+
+
+    fun testParseMetaDataFromReader() {
+        val html = """
+            <html>
+            <head>
+        <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="alternate" href="https://animepahe.ru" hreflang="en-us">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="theme-color" content="#373a3c"><!-- Chrome, Firefox OS, Opera and Vivaldi -->
+    <meta name="msapplication-navbutton-color" content="#373a3c"><!-- Windows Phone -->
+    <meta name="apple-mobile-web-app-status-bar-style" content="#373a3c"><!-- iOS Safari -->
+    <meta http-equiv="x-dns-prefetch-control" content="on">
+    <link rel="preconnect" href="//i.animepahe.ru">
+    <link rel="preload" href="/app/fonts/QldONTRRphEb_-V7LB6xTA.woff2" as="font" type="font/woff2" crossorigin>
+    <link rel="preload" href="/app/css/bootstrap.min.css" as="style">
+    <link rel="preload" href="/app/css/fork-awesome.min.css" as="style">
+    <link rel="preload" href="/app/css/style.css" as="style">
+    <link rel="preload" href="/app/js/vendor/bootstrap.bundle.min.js" as="script">
+    <link rel="preload" href="/app/js/core.js" as="script">
+    <meta name="msapplication-TileImage" content="https://animepahe.ru/animepahe-270x270.png">
+    <title>animepahe :: okay-ish anime website</title>
+    <meta name="description" content="Watch or download anime shows in HD 720p/1080p.">
+    <meta name="keywords" content="Anime,Pahe,Mini,720p,HD,mp4,English,Subtitle,Hardsub">
+    <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1">
+    <meta property="og:site_name" content="animepahe">
+    <meta property="og:locale" content="en_US">
+    <meta property="og:image" content="https://animepahe.ru/animepahe-270x270.png">
+    <meta property="og:url" content="https://animepahe.ru">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="cloud anime encoding">
+    <meta property="og:description" content="Watch or download anime shows in HD 720p/1080p.">
+    <meta name="author" content="animepahe">
+    <link href="/apple-touch-icon.png" rel="apple-touch-icon-precomposed" type="image/png">
+    <link href="/apple-touch-icon.png" rel="shortcut icon">
+    <link href="/pikacon-32x32.png" rel="shortcut icon" type="image/png">
+    <link href="/pikacon.ico" rel="shortcut icon" type="image/x-icon">
+    <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="https://animepahe.ru/feed">
+    <link rel="stylesheet" href="/app/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/app/css/fork-awesome.min.css">
+    <link rel="stylesheet" href="/app/css/style.css">
+    </head>
+    </html>
+        """.trimIndent()
+
+        val metaData = Ksoup.parseMetaData(html, "https://animepahe.ru/")
+        assertEquals("animepahe :: okay-ish anime website", metaData.title)
+        assertEquals("cloud anime encoding", metaData.ogTitle)
+        assertEquals("https://animepahe.ru/animepahe-270x270.png", metaData.ogImage)
+        assertEquals("https://animepahe.ru/apple-touch-icon.png", metaData.shortcutIcon)
     }
 
 }
