1504374bdf6c0366e1f71e1694195d4274238cb5
Sabeeh Ul Hussnain <mr.hussnain.it@gmail.com>
2025-04-26 01:53:46 +0500

    Align arrow spacing and trim whitespace

--- ksoup/src/com/fleeksoft/ksoup/select/NodeVisitor.kt
+++ ksoup/src/com/fleeksoft/ksoup/select/NodeVisitor.kt
@@ -15,7 +15,7 @@ Node visitor interface. Provide an implementing class to {@link NodeTraversor} o
 to iterate through nodes.
 <p>
 This interface provides two methods, {@link #head} and {@link #tail}. The head method is called when the node is first
-seen, and the tail method when all of the node's children have been visited. As an example, {@code head} can be used to
+seen, and the tail method when all the node's children have been visited. As an example, {@code head} can be used to
 emit a start tag for a node, and {@code tail} to create the end tag. The {@code tail} method defaults to a no-op, so
 the {@code head} method is the {@link FunctionalInterface}.
 </p>
@@ -23,23 +23,23 @@ the {@code head} method is the {@link FunctionalInterface}.
 <pre><code>
 doc.body().traverse((node, depth) -> {
 switch (node) {
-case Element el     -> print(el.tag() + ": " + el.ownText());
-case DataNode data  -> print("Data: " + data.getWholeData());
-default             -> print(node.nodeName() + " at depth " + depth);
+case Element el -> print(el.tag() + ": " + el.ownText());
+case DataNode data -> print("Data: " + data.getWholeData());
+default -> print(node.nodeName() + " at depth " + depth);
 }
 });
 </code></pre>
  */
 public fun interface NodeVisitor {
     /**
-     Callback for when a node is first visited.
-     <p>The node may be modified (e.g. {@link Node#attr(String)}, replaced {@link Node#replaceWith(Node)}) or removed
-     {@link Node#remove()}. If it's {@code instanceOf Element}, you may cast it to an {@link Element} and access those
-     methods.</p>
+    Callback for when a node is first visited.
+    <p>The node may be modified (e.g. {@link Node#attr(String)}, replaced {@link Node#replaceWith(Node)}) or removed
+    {@link Node#remove()}. If it's {@code instanceOf Element}, you may cast it to an {@link Element} and access those
+    methods.</p>
 
-     @param node the node being visited.
-     @param depth the depth of the node, relative to the root node. E.g., the root node has depth 0, and a child node
-     of that will have depth 1.
+    @param node the node being visited.
+    @param depth the depth of the node, relative to the root node. E.g., the root node has depth 0, and a child node
+    of that will have depth 1.
      */
     public fun head(
         node: Node,
