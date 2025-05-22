38d787a0716e82fc57f807c6ffa339337cfa2b53
Sabeeh Ul Hussnain <mr.hussnain.it@gmail.com>
2025-05-23 02:01:22 +0500

    Refactor `provideHttpClientEngine` to return engine factory
    Updated the `provideHttpClientEngine` function across all platforms to return `HttpClientEngineFactory<HttpClientEngineConfig>` instead of creating an engine instance. This aligns with Ktor's recommended patterns for better flexibility and configurability.

--- ksoup-network/src@wasm/com/fleeksoft/ksoup/network/ProvideHttpClientEngineWasm.kt
+++ ksoup-network/src@wasm/com/fleeksoft/ksoup/network/ProvideHttpClientEngineWasm.kt
@@ -3,6 +3,6 @@ package com.fleeksoft.ksoup.network
 import io.ktor.client.engine.*
 import io.ktor.client.engine.js.*
 
-actual fun provideHttpClientEngine(): HttpClientEngine {
-    return Js.create()
+actual fun provideHttpClientEngine(): HttpClientEngineFactory<HttpClientEngineConfig> {
+    return Js
 }
