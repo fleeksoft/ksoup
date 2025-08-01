c2a21333892535d377b750ed2ca493c2856323fe
Sabeeh Ul Hussnain <mr.hussnain.it@gmail.com>
2025-08-02 02:16:22 +0500

    docs: deprecate `PseudoTextElement` in favor of `Element#selectNodes`
    * Marked `PseudoTextElement` as `@Deprecated` with guidance to use `Element#selectNodes(String, Class)` instead, utilizing `::textnode` and `TextNode`.

--- ksoup/src/com/fleeksoft/ksoup/nodes/PseudoTextElement.kt
+++ ksoup/src/com/fleeksoft/ksoup/nodes/PseudoTextElement.kt
@@ -15,6 +15,8 @@ import com.fleeksoft.ksoup.parser.Tag
  * Represents a [TextNode] as an [Element], to enable text nodes to be selected with
  * the [com.fleeksoft.ksoup.select.Selector] `:matchText` syntax.
  */
+
+@Deprecated("use {@link Element#selectNodes(String, Class)} instead, with selector of <code>::textnode</code> and class <code>TextNode</code>.")
 internal class PseudoTextElement(tag: Tag, baseUri: String?, attributes: Attributes?) :
     Element(tag, baseUri, attributes) {
     override fun outerHtmlHead(accum: QuietAppendable, out: Document.OutputSettings) {
